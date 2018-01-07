230a4c6a77b5724937aff3f137fae324985a5f6d
Алексей Белов <Alenorze@gmail.com>
2018-01-07 16:31:18 +0200

    Angular Routing in Django URLs

--- src/lnrzapi/urls.py
+++ src/lnrzapi/urls.py
@@ -10,11 +10,14 @@ schema_view = get_swagger_view(title='Pastebin API')
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='ng.html'), name='home'),
     url(r'^api/$', schema_view),
     url(r'^admin/', admin.site.urls),
     url(r'^api/auth/', include('accounts.api.urls', namespace='api-auth')),
     url(r'^api/user/', include('accounts.api.user.urls', namespace='api-user')),
     url(r'^api/status/', include('status.api.urls', namespace='api-status')),
     url(r'^api/postings/', include('postings.api.urls', namespace='api-postings')),
+]
+
+urlpatterns += [
+    url(r'^(?P<path>.*)', TemplateView.as_view(template_name='ng.html'), name='home'),
 ]
