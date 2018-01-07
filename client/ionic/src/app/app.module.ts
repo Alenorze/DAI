36b697325245e66c4ac49ca1d329809155f5575d
Алексей Белов <Alenorze@gmail.com>
2018-01-07 16:39:36 +0200

    URL Route Parameters

--- client/ionic/src/app/app.module.ts
+++ client/ionic/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StatusComponent } from './status/status.component';
+import { StatusCreateComponent } from './status-create/status-create.component';
+import { StatusDetailComponent } from './status-detail/status-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatusComponent
+    StatusComponent,
+    StatusCreateComponent,
+    StatusDetailComponent
   ],
   imports: [
     AppRoutingModule,
